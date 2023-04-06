fa5b1d0f4eaa9b62d0b888003ec9d75dc4f0b653
Ruslan Omalaev <89082306+Starbreaker84@users.noreply.github.com>
2023-04-06 17:22:24 +0300

    Update lesson-04.fs

--- lesson-04.fs
+++ lesson-04.fs
@@ -1,17 +1,17 @@
 // 7.1.1
 let rec fibo = function 
- | 1 -> 0
- | 2 -> 1
- | n when n < 1 -> 0
- | n -> fibo(n - 2) + fibo(n - 1)
+  | 1 -> 0 
+  | 2 -> 1 
+  | n when n < 1 -> 0 
+  | n -> fibo(n - 2) + fibo(n - 1)
 
 // 7.1.2
 let rec sum = function 
- | 1 -> 1
- | n when n < 1 -> 0
- | n -> n + sum(n - 1)
+  | 1 -> 1 
+  | n when n < 1 -> 0 
+  | n -> sum(n-1) + n 
 
 // 7.1.3
-let rec sum2 = function 
- | (m,0) -> m 
- | (m,n) -> m + n + sum2(m, n - 1)
+let rec sum2 = function  
+  | (m,0) -> m  
+  | (m,n) -> m + n + sum2(m, n - 1)
