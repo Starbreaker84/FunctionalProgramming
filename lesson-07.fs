e8c92537f37cba7aca342c699ff5a7a474bbbdf4
Ruslan Omalaev <89082306+Starbreaker84@users.noreply.github.com>
2023-04-07 16:23:28 +0300

    Update lesson-07.fs

--- lesson-07.fs
+++ lesson-07.fs
@@ -6,7 +6,17 @@ let rec pow = function
   | (s, n) -> s + pow (s, n - 1)
 
 // 17.2
-let rec isIthChar  ...
+let rec isIthChar = function
+    | (s, n, c) when n > (String.length s) - 1 -> false
+    | (s, n, c) when n < 0 -> false
+    | (s, n, c) when s.[n] = c -> true
+    | (s, n, c) -> false
 
 // 17.3
-let rec occFromIth  ...
+let occFromIth (s, n, c) = 
+    let rec check = function
+        | (n, c, acc) when n > (String.length s) - 1 -> acc
+        | (n, c, acc) when n < 0 -> 0
+        | (n, c, acc) when c = s.[n] -> check (n + 1, c, acc + 1)
+        | (n, c, acc) -> check (n + 1, c, acc)
+    check (n, c, 0).
