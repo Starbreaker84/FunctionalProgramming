39187b3551fceed9dc85e6ddeb51598b9aa45afb
Ruslan Omalaev <89082306+Starbreaker84@users.noreply.github.com>
2023-05-22 11:45:49 +0300

    Update lesson-22.fs

--- lesson-22.fs
+++ lesson-22.fs
@@ -1,18 +1,18 @@
 // 48.4.1
-let rec fib1 n n1 n2 = 
+let rec fibo1 n n1 n2 = 
     match n with
-    | 0 -> n2
     | 1 -> n1
-    | n -> fib1 (n - 1) (n1 + n2) n1
+    | _ -> fibo1 (n-1) (n1+n2) n1
 
 // 48.4.2
-let rec fib2 n c = 
+let rec fibo2 n c = 
     match n with
     | 0 -> c 0
     | 1 -> c 1
-    | n -> fib2 (n - 2) (fun a -> fib2 (n - 1) (fun b -> c (a + b)))
+    | _ -> (fibo2 (n-1) (fun x -> c x) + fibo2 (n-2) (fun y -> c y))
 
 // 48.4.3
-let rec bigList n k =
-  if n = 0 then k []
-  else bigList (n - 1) (fun res -> k(1::res))
+let rec bigList n k = 
+    match n with
+    | 0 -> k []
+    | _ -> bigList (n-1) (fun accum -> k(1::accum))
